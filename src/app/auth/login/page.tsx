dfac5bceea1e6a6720fee725b521dfee7d25015f
Stiven Tovar <stivent367@gmail.com>
2024-03-11 18:49:15 -0500

    Update metadata

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,4 +1,9 @@
 import LoginPage from "@/screens/auth/login/Login";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Welcome',
+}
 
 export default function SignUp() {
   return (
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,4 +1,9 @@
 import SignUpPage from "@/screens/auth/signUp/signup";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Create an account',
+}
 
 export default function SignUp() {
   return (
